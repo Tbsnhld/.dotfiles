b34bcd0c0836d330813321d3e2734451d88afa26
Tobias Neuhold <tobias.neuhold@outlook.de>
2023-09-22 00:54:13 +0200

    fixed bad layout

--- lua/tobi/plugin_config/harpoon.lua
+++ lua/tobi/plugin_config/harpoon.lua
@@ -3,7 +3,7 @@ local ui = require("harpoon.ui")
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-h>", ui.toggle_quick_menu)
-vim.keymap.set("n", "<C-ch>", mark.clear_all)
+vim.keymap.set("n", "<leader>hc", mark.clear_all)
 
 
 vim.keymap.set("n", "<C-m>", function() ui.nav_file(1) end)
