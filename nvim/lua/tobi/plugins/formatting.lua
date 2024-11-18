59c1072a1b16687fc0e592be6e7f4e5f1a3b3d23
Tobias Neuhold <Tobias.neuhold@dmc.valantic.com>
2024-11-18 15:20:37 +0100

    current mac

--- nvim/lua/tobi/plugins/formatting.lua
+++ nvim/lua/tobi/plugins/formatting.lua
@@ -5,7 +5,7 @@ return {
         local conform = require("conform")
         conform.setup({
             formatters_by_ft = {
-                --apex = { "prettier" },
+                apex = { "prettier" },
                 lua = { "stylua" },
             },
             format_on_save = {
--- nvim/lua/tobi/plugins/mini.lua
+++ nvim/lua/tobi/plugins/mini.lua
@@ -1,15 +1,10 @@
 return {
     {
-        'echasnovski/mini.pairs', version = false ,
+        'echasnovski/mini.pairs',
+        version = false,
         config = function()
             require('mini.pairs').setup()
         end,
     },
-    {
-        'echasnovski/mini.animate', version = false,
-        config = function()
-            require('mini.animate').setup()
-        end,
-    }
 
 }
