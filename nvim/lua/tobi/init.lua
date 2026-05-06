23cc2b3b961971bf676a3be8753019b4c9e5da4d
Tobias <Tobias.neuhold@outlook.de>
2026-05-07 01:23:53 +0200

    fixed for v.12

--- nvim/lua/tobi/init.lua
+++ nvim/lua/tobi/init.lua
@@ -1,4 +1,21 @@
-require("tobi.set")
-require("tobi.remap")
+require("tobi.config.set")
+require("tobi.config.remap")
 
-require("tobi.lazy_init")
+require("tobi.config.lazy_init")
+
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'python, rust, lua' },
+  callback = function()
+    vim.treesitter.start()
+  end,
+})
+
+vim.lsp.config['lua_ls'] = {
+  cmd = { 'lua-language-server' },
+  filepytes = { 'lua' },
+  root_markers = { '.luarc.json', '.git' },
+  settings = { Lua = { diagnostics = { globals = { "vim " } } } }
+}
+
+vim.lsp.enable('lua_ls')
--- nvim/lua/tobi/plugins/init.lua
+++ nvim/lua/tobi/plugins/init.lua
@@ -1,9 +1,52 @@
 return {
-    -- put simple plugins here that don't need configuragion
-    {
-        'mrcjkb/rustaceanvim',
-        version = '^5',
-        lazy = false
-    }
+  -- Rust
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^5',
+    lazy = false,
+  },
 
+  -- Completion
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lua',
+      'saadparwaiz1/cmp_luasnip',
+
+      -- Snippets
+      'L3MON4D3/LuaSnip',
+      'rafamadriz/friendly-snippets',
+    },
+
+    config = function()
+      local cmp = require('cmp')
+      local luasnip = require('luasnip')
+
+      require("luasnip.loaders.from_vscode").lazy_load()
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+
+        mapping = cmp.mapping.preset.insert({
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ['<Tab>'] = cmp.mapping.select_next_item(),
+          ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+        }),
+
+        sources = {
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+          { name = 'buffer' },
+          { name = 'path' },
+        },
+      })
+    end,
+  },
 }
